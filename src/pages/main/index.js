2ced691d1fd8db569b41d71c70e5da5d89b99e9b
João Füzessy <joao.fuzessy@dtidigital.com.br>
2019-09-03 01:40:36 -0300

    Criação de arrays estruturados  com os agrupamentos por transaction_id

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,16 +3,80 @@ import api from '../../services/api';
 import Timeline from '../../components/Timeline';
 
 class Main extends Component{
+    
+    state ={
+        compras: [],
+    }
+    
+    
     componentDidMount(){
         this.loadTimeline();
     }
 
     loadTimeline = async () => {
         const resp = await api.get('/events.json');
-        console.log(resp.data.events);
+        const structure = resp.data.events;
+        
+
+        const checkTransactionId = (data) =>{
+            let transactionID;
+            data.forEach(element => {
+                        
+                if(element.key=="transaction_id"){
+                    transactionID = element.value;
+                    
+                }
+                
+            });
+            return transactionID;
+        }
+
+
+        const structureData = (structure) =>{ 
+            const comprasArray = [];
+            
+            structure.map((e)=>{
+                
+                
+                if (e.event == "comprou"){
+                    
+                    let transactionID = checkTransactionId(e.custom_data);
+                    const itensCompra=[];
+                    
+                    structure.map((e)=>{
+                        
+                        let transactionID2 = checkTransactionId(e.custom_data);
+                        if(e.event != "comprou" && transactionID==transactionID2){
+                            itensCompra.push(e);
+                            
+                        }
+                    })
+                    
+                    e.outros = itensCompra;
+                    
+                    comprasArray.push(e);
+ 
+                }
+                
+            } )
+            console.log(comprasArray);
+            return comprasArray;
+        } 
+
+
+        this.setState({compras: structureData(structure)});
     }
+
+    
+
     render(){
-        return <Timeline></Timeline>;
+        return (
+            <div>
+                {this.state.compras.map(event => (
+                    <h1 key={event.timestamp}>{event.event}</h1>
+                ))}
+            </div>
+        );
     }
 }
 
