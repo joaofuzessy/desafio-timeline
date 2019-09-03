f94d678cb09bdef333a9dc196d1e158abc0dca56
João Füzessy <joao.fuzessy@dtidigital.com.br>
2019-09-03 02:09:02 -0300

    Mudança da lógica de Main para Timeline

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,37 +1,115 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Timeline.scss';
+import api from '../../services/api';
 
-const Timeline = () =>{
-    return(
-    <ul>
-        <li>
-            <div className="Card">
-            <div className="CardHeader">
-            <ul>
-                <li>Dia</li>
-                <li>Hora</li>
-                <li>Local</li>
-                <li>Valor Total</li>
-            </ul>
+class Timeline extends Component{
+    
+    state = {
+        compras: [],
+    }
+    
+    
+    componentDidMount(){
+        this.loadTimeline();
+    }
+
+    loadTimeline = async () => {
+        const resp = await api.get('/events.json');
+        const structure = resp.data.events;
+        
+        const checkTransactionId = (data) =>{
+            let transactionID;
+            data.forEach(element => {
+                        
+                if(element.key==="transaction_id"){
+                    transactionID = element.value;             
+                }
+                
+            });
+            return transactionID;
+        }
+
+
+        const structureData = (structure) =>{ 
+            const comprasArray = [];
+            
+            structure.map((e)=>{ // Itera a primeira vez para achar os obj do tipo "comprou"
+                
+                if (e.event === "comprou"){
+                    
+                    let transactionIdloja = checkTransactionId(e.custom_data);
+                    const itensCompra=[];
+
+                    structure.map((e)=>{ // Itera novamente para verificar os produtos("comprou-produto") com o mesmo Transaction ID
+                        
+                        let transactionIdProduto = checkTransactionId(e.custom_data);
+                        
+                        if(e.event === "comprou-produto" && transactionIdloja === transactionIdProduto){
+                            itensCompra.push(e);
+                            
+                        }
+                    })
+                    
+                    e.produtos = itensCompra;
+                    
+                    comprasArray.push(e);
+ 
+                }
+                
+            })
+            console.log(comprasArray);
+            return comprasArray;
+        } 
+
+
+        this.setState({compras: structureData(structure)});
+    }
+
+    
+
+    render(){
+        return (
+            <div>
+                {this.state.compras.map(event => (
+                    <h1 key={event.timestamp}>{event.event}</h1>
+                ))}
             </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Produto</th>
-                        <th>Preço</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Camisa azul</td>
-                        <td>R$100,00</td>
-                    </tr>
-                </tbody>
-            </table>
-            </div>  
-        </li>
-    </ul>
-    );
+        );
+    }
 }
+
+// const Timeline = () =>{
+//     return(
+//     <ul>
+//         <li>
+//             <div className="Card">
+//             <div className="CardHeader">
+//             <ul>
+//                 <li>Dia</li>
+//                 <li>Hora</li>
+//                 <li>Local</li>
+//                 <li>Valor Total</li>
+//             </ul>
+//             </div>
+//             <table>
+//                 <thead>
+//                     <tr>
+//                         <th>Produto</th>
+//                         <th>Preço</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     <tr>
+//                         <td>Camisa azul</td>
+//                         <td>R$100,00</td>
+//                     </tr>
+//                 </tbody>
+            
+//             </table>
+//             </div>  
+//         </li>
+//     </ul>
+//     );
+// }
 
 export default Timeline;
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,83 +1,10 @@
-import React, { Component } from 'react';
-import api from '../../services/api';
+import React from 'react';
+import TimeLine from '../../components/Timeline';
 
-
-class Main extends Component{
-    
-    state ={
-        compras: [],
-    }
-    
-    
-    componentDidMount(){
-        this.loadTimeline();
-    }
-
-    loadTimeline = async () => {
-        const resp = await api.get('/events.json');
-        const structure = resp.data.events;
-        
-
-        const checkTransactionId = (data) =>{
-            let transactionID;
-            data.forEach(element => {
-                        
-                if(element.key==="transaction_id"){
-                    transactionID = element.value;
-                    
-                }
-                
-            });
-            return transactionID;
-        }
-
-
-        const structureData = (structure) =>{ 
-            const comprasArray = [];
-            
-            structure.map((e)=>{ // Itera a primeira vez para achar os obj do tipo "comprou"
-                
-                if (e.event === "comprou"){
-                    
-                    let transactionIdloja = checkTransactionId(e.custom_data);
-                    const itensCompra=[];
-
-                    structure.map((e)=>{ // Itera novamente para verificar os produtos("comprou-produto") com o mesmo Transaction ID
-                        
-                        let transactionIdProduto = checkTransactionId(e.custom_data);
-                        
-                        if(e.event === "comprou-produto" && transactionIdloja === transactionIdProduto){
-                            itensCompra.push(e);
-                            
-                        }
-                    })
-                    
-                    e.produtos = itensCompra;
-                    
-                    comprasArray.push(e);
- 
-                }
-                
-            })
-            console.log(comprasArray);
-            return comprasArray;
-        } 
-
-
-        this.setState({compras: structureData(structure)});
-    }
-
-    
-
-    render(){
-        return (
-            <div>
-                {this.state.compras.map(event => (
-                    <h1 key={event.timestamp}>{event.event}</h1>
-                ))}
-            </div>
-        );
-    }
+const Main = () =>{
+    return(
+        <TimeLine></TimeLine>
+    )
 }
 
 export default  Main;
